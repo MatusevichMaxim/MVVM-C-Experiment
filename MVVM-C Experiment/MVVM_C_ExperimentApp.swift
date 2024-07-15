f33fd9a421a7a26d0cc4bb5289e5645d89beb99f
Maksim Matusevich <imulir@gmail.com>
2024-07-15 11:55:35 -0700

    Initial Commit

---
+++ MVVM-C Experiment/MVVM_C_ExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_C_ExperimentApp.swift
+//  MVVM-C Experiment
+//
+//  Created by Maksim Matusevich on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_C_ExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
